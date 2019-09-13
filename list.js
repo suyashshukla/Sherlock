cbf8e8c68d8d9e2218e180e9f697515481994945
suyashshukla <suyashshukla1997@gmail.com>
2019-09-13 19:48:07 +0530

    Advancing

--- list.js
+++ list.js
@@ -1,60 +1,64 @@
 console.log("entered");
 
+$(".tasks").hover(function (){
+    $(".dropdown").style.display="block";
+});
+
 var slideIndex = 1;
-    
+
+showSlides(slideIndex);
+
+function plus(inc) {
+    slideIndex = (slideIndex + inc);
     showSlides(slideIndex);
+}
 
-  function plus(inc){
-      slideIndex = (slideIndex+inc);
-        showSlides(slideIndex);
-    }
-
-function openNav(){
+function openNav() {
     document.getElementById("mysidenav").style.width = "300px";
 }
 
-function closeNav(){
+function closeNav() {
     document.getElementById("mysidenav").style.width = "0px";
 }
 
-function showSlides(n){
+function showSlides(n) {
     slideIndex = n;
 
-    if(slideIndex>3){
-        slideIndex = 1;  
+    if (slideIndex > 3) {
+        slideIndex = 1;
+    }
+
+    if (slideIndex < 1) {
+        slideIndex = 3;
+    }
+
+    var slide = document.getElementsByClassName("mySlides");
+
+
+    for (var i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
+    }
+
+    slide[slideIndex - 1].style.display = "block";
+
 }
 
-if(slideIndex<1){
-    slideIndex = 3;
-}
-    
-var slide = document.getElementsByClassName("mySlides");
+function showSlides() {
 
+    var slides = document.getElementsByClassName("mySlides");
 
-for(var i = 0;i<slide.length;i++){
-    slide[i].style.display = "none";
-}
+    slideIndex++;
 
-slide[slideIndex-1].style.display = "block";
+    if (slideIndex > 3) {
+        slideIndex = 1;
+    }
 
-}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-function showSlides(){
+    slides[slideIndex - 1].style.display = "block";
 
-var slides = document.getElementsByClassName("mySlides");
-
-slideIndex++;
-
-if(slideIndex>3){
-  slideIndex = 1;  
-}
-
-for(i = 0;i<slides.length;i++){
-    slides[i].style.display = "none";
-}
-
-slides[slideIndex-1].style.display = "block";
-
-setTimeout(showSlides,2000);
+    setTimeout(showSlides, 2500);
 
 }
