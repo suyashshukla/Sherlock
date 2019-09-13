e778597f02c8bf34c6a9da532efb8b7ab37de22f
suyashshukla <suyashshukla1997@gmail.com>
2019-09-13 13:48:36 +0530

    Jumping

--- list.js
+++ list.js
@@ -1,9 +1,60 @@
-angular.module('apps',[])
-.component('stuff',
-{
-template : 
-'<h1>{{$ctrl.data}}</h1>',
-controller : function List(){
-    this.data = 'Hello World!'
+console.log("entered");
+
+var slideIndex = 1;
+    
+    showSlides(slideIndex);
+
+  function plus(inc){
+      slideIndex = (slideIndex+inc);
+        showSlides(slideIndex);
+    }
+
+function openNav(){
+    document.getElementById("mysidenav").style.width = "300px";
 }
-});
+
+function closeNav(){
+    document.getElementById("mysidenav").style.width = "0px";
+}
+
+function showSlides(n){
+    slideIndex = n;
+
+    if(slideIndex>3){
+        slideIndex = 1;  
+}
+
+if(slideIndex<1){
+    slideIndex = 3;
+}
+    
+var slide = document.getElementsByClassName("mySlides");
+
+
+for(var i = 0;i<slide.length;i++){
+    slide[i].style.display = "none";
+}
+
+slide[slideIndex-1].style.display = "block";
+
+}
+
+function showSlides(){
+
+var slides = document.getElementsByClassName("mySlides");
+
+slideIndex++;
+
+if(slideIndex>3){
+  slideIndex = 1;  
+}
+
+for(i = 0;i<slides.length;i++){
+    slides[i].style.display = "none";
+}
+
+slides[slideIndex-1].style.display = "block";
+
+setTimeout(showSlides,2000);
+
+}
