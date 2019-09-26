d63a5845383dc677f40396546a16c54db969bf21
suyashshukla <suyashshukla1997@gmail.com>
2019-09-26 18:57:33 +0530

    Task Box

--- AddressBook/script.js
+++ AddressBook/script.js
@@ -12,8 +12,16 @@ function openLightBox() {
 
 function closeLightBox() {
 
+    var inputs = $("input");
+    var address = $("textarea");
+
+    // for (var i = 0; i < inputs.length; i++) {
+    //     console.log(inputs[i].innerText);
+    // }
+    // console.log(address.prop("innerText"));
+
     var box = document.getElementById("content-add-address");
-    box.style.display = "none";
+    // box.style.display = "none";
 
 }
 
